2c6c3f973fd5545f533dd8507b2fb22c33201782
Crowdbotics <team@crowdbotics.com>
2023-11-15 13:12:58 +0000

    Initial commit

---
+++ backend/td_bank_44558/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for td_bank_44558 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "td_bank_44558.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "td-bank-44558",
+  url: "https://td-bank-44558.botics.co",
+  api: "https://td-bank-44558.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
