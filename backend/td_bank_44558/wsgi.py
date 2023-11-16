e3892dcba6a64913acff211d17447ca8cdec92fa
crowdbotics-apps <team@crowdbotics.com>
2023-11-16 20:24:10 +0000

    Update models

--- backend/td_bank_44558/wsgi.py
+++ backend/td_bank_44558/wsgi.py
@@ -11,8 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "td_bank_44558.settings"
-)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "td_bank_44558.settings")
 
 application = get_wsgi_application()
